a9a2ed071cfa4d695de08a8b9dc0e1d4e3a36d21
Chou Cheng-Yao <anti.ning@gmail.com>
2018-10-03 16:07:31 +0800

    meun

--- main.cpp
+++ main.cpp
@@ -9,20 +9,68 @@
 #include <iostream>
 #include "Encryptor.h"
 using std::cout;
+using std::cin;
 using std::endl;
 
 int main(int argc, const char * argv[]) {
     Encryptor encryptor;
-    /*
-    string plainText = "keepgoingnevergiveup";
-    cout << encryptor.Enc("Caesar", plainText, 7) << endl;
-    cout << encryptor.Enc("Monoalphabetic", plainText, "qwertyuiopasdfghjklzxcvbnm") << endl;
-    cout << encryptor.Enc("Playfair","hidethegoldinthetreestump","playfairexample") << endl;
-    
-    cout << encryptor.Enc("Vernam","helo","xmcl") << endl;
-    cout << encryptor.Enc("RowTrans", "wearediscoveredfleeatonce", "632415") << endl;*/
-    
-    vector<int> key = {15,11,19,18,16,3,7,14,2,20,4,12,9,6,1,5,17,13,10,8};
-    cout << encryptor.Enc("Product", "thestrengthofthispig", key) << endl;
+    string cmd,plainText;
+    int method,iKey,key_size;
+    string sKey;
+    vector<int> aKey;
+    cout << "E)Encrypt\n";
+    cout << "D)Decrypt\n";
+    while(cin >> cmd){
+        if(cmd == "E"){
+            cout << "1) Caesar Cipher\n";
+            cout << "2) Monoalphabetic Cipher\n";
+            cout << "3) Playfair Cipher\n";
+            cout << "4) Vernam Cipher\n";
+            cout << "5) Transposition Cipher\n";
+            cout << "6) Product Cipher\n";
+            cin >> method;
+            
+            switch (method) {
+                case 1:
+                    
+                    cin >> plainText >> iKey;
+                    cout << encryptor.Enc("Caesar", plainText, iKey) << endl;
+                    break;
+                case 2:
+                    cin  >> plainText >> sKey;
+                    cout << encryptor.Enc("Monoalphabetic",plainText,sKey)<< endl;
+                    break;
+                case 3:
+                    cin  >> plainText >> sKey;
+                    cout << encryptor.Enc("Playfair",plainText,sKey)<< endl;
+                    break;
+                case 4:
+                    cin  >> plainText >> sKey;
+                    cout << encryptor.Enc("Vernam",plainText,sKey)<< endl;
+                    break;
+                case 5:
+                    cin  >> plainText >> sKey;
+                    cout << encryptor.Enc("RowTrans",plainText,sKey)<< endl;
+                    break;
+                case 6:
+                    cin >> plainText >> key_size;
+                    aKey.resize(key_size);
+                    for(int i=0;i<key_size;++i)
+                        cin >> aKey[i];
+                    cout << encryptor.Enc("Product", plainText,aKey)<< endl;
+                default:
+                    cout << "Wrong input\n";
+                    break;
+            }
+            
+        }else if(cmd == "D"){
+            // you code here
+        }else{
+            cout << "exit\n";
+            break;
+        }
+        cout << "E)Encrypt\n";
+        cout << "D)Decrypt\n";
+    }
     return 0;
 }
